49d044cc9fd5ae097be8c6ab2fc0b77b772ec6ac
Vasil Raev <Xizario@gmail.com>
2018-07-31 10:53:22 +0300

    Update events used in GridContainer

--- src/components/GridContainer.js
+++ src/components/GridContainer.js
@@ -5,73 +5,73 @@ import MyCommandCell from './MyCommandCell'
 import { Grid, GridColumn as Column, GridToolbar } from '@progress/kendo-react-grid';
 
 class GridContainer extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.remove = this.remove.bind(this);
-        this.CommandCell = MyCommandCell(this.remove);
-        this.addItem = this.addItem.bind(this);
-        this.rowClick = this.rowClick.bind(this)
-    }
+    this.remove = this.remove.bind(this);
+    this.CommandCell = MyCommandCell(this.remove);
+    this.addItem = this.addItem.bind(this);
+    this.rowClick = this.rowClick.bind(this)
+  }
 
-    addItem() {
-        this.props.addProduct();
-    }
+  addItem() {
+    this.props.addProduct();
+  }
 
-    remove(dataItem) {
-        this.props.productDeleted(dataItem);
-    }
+  remove(dataItem) {
+    this.props.productDeleted(dataItem);
+  }
 
-    rowClick(event) {
-        this.props.changeSelected(event.dataItem);
-    }
+  rowClick(event) {
+    this.props.changeSelected(event.dataItem);
+  }
 
-    render() {
-        return (
-            <div className="grid-container col-md-8 col-sm-12 col-xs-12">
-                <div className="header">
-                    <h5>Data</h5>
-                </div>
-                <Grid
-                    rowClick={this.rowClick}
-                    style={{ maxHeight: "600px" }}
-                    data={this.props.products}
-                    rowRender={(row, dataItem) => dataItem["ProductID"] === this.props.selectedRow
-                        ? React.cloneElement(
-                            row,
-                            { ...row.props, style: {backgroundColor: "#ff6b58", color: "white" } },
-                            row.props.children
-                            )
-                        : row
-                    }
-                >
-                    <GridToolbar>
-                        <button
-                            title="Add new"
-                            className="k-button k-primary"
-                            onClick={this.addItem}
-                            disabled={this.props.isNew}
-                        >
-                            Add new
+  render() {
+    return (
+      <div className="grid-container col-md-8 col-sm-12 col-xs-12">
+        <div className="header">
+          <h5>Data</h5>
+        </div>
+        <Grid
+          onRowClick={this.rowClick}
+          style={{ maxHeight: "600px" }}
+          data={this.props.products}
+          rowRender={(row, props) => props.dataItem["ProductID"] === this.props.selectedRow
+            ? React.cloneElement(
+              row,
+              { ...row.props, style: { backgroundColor: "#ff6b58", color: "white" } },
+              row.props.children
+            )
+            : row
+          }
+        >
+          <GridToolbar>
+            <button
+              title="Add new"
+              className="k-button k-primary"
+              onClick={this.addItem}
+              disabled={this.props.isNew}
+            >
+              Add new
                         </button>
-                    </GridToolbar>
-                    <Column field="ProductID" title="ID" width="40px" />
-                    <Column field="ProductName" title="Name" width="150px" />
-                    <Column field="UnitPrice" title="Price" width="80px" />
-                    <Column field="UnitsInStock" title="In stock" width="80px" />
-                    <Column cell={this.CommandCell} width="120px" />
-                </Grid>
-            </div>
-        );
-    }
+          </GridToolbar>
+          <Column field="ProductID" title="ID" width="40px" />
+          <Column field="ProductName" title="Name" width="150px" />
+          <Column field="UnitPrice" title="Price" width="80px" />
+          <Column field="UnitsInStock" title="In stock" width="80px" />
+          <Column cell={this.CommandCell} width="120px" />
+        </Grid>
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-    return {
-        products: state.productsReducer.products,
-        isNew: state.selectionReducer.isNew,
-        selectedRow: state.selectionReducer.selectedRow
-    }
+  return {
+    products: state.productsReducer.products,
+    isNew: state.selectionReducer.isNew,
+    selectedRow: state.selectionReducer.selectedRow
+  }
 }
 
 export default connect(mapStateToProps, { productUpdated, productDelete })(GridContainer);
