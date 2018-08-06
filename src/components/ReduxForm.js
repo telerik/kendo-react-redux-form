6e4423c591eaf52c2d4c28c34fbf36556d51f42b
Vladimir Iliev <vladimir@ticktech.bg>
2018-08-06 13:59:57 +0300

    refactor: simplify code

--- src/components/ReduxForm.js
+++ src/components/ReduxForm.js
@@ -1,21 +1,38 @@
 import React from 'react'
 import { Field, reduxForm } from 'redux-form'
 import { connect } from 'react-redux';
+import { Input, NumericTextBox, Switch } from '@progress/kendo-react-inputs'
+import { DatePicker } from '@progress/kendo-react-dateinputs'
+import { DropDownList } from '@progress/kendo-react-dropdowns';
+import { Button } from '@progress/kendo-react-buttons';
 
-const required = value => value ? undefined : 'Required*'
+const required = value => value ? undefined : 'Required*';
 
 const minValue = min => value =>
-  value && value < min ? `Must be at least ${min}` : undefined
-const minValue0 = minValue(0)
-const minValue1 = minValue(1)
+  value && value < min ? `Must be at least ${min}` : undefined;
+const minValue0 = minValue(0);
+const minValue1 = minValue(1);
 
-const renderField = ({ input, label, type, meta: { touched, error, warning } }) => (
-  <label className="k-form-field">
-    {label !== "ProductID" ? <span>{label}</span> : ""}
-    <input {...input} placeholder={label} type={type} className={label === "ProductID" ? "hidden" : "k-textbox"} />
-    {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
-  </label>
-)
+const kendoHOC = (type, {input: {value, onChange, onBlur}, meta, label, ...rest}) => {
+    const Type = type;
+
+    return <label className="k-form-field">
+        <span>{label}</span>
+        <Type
+            value={value}
+            onChange={(event) => onChange(event.value)}
+            onBlur={(event) => onBlur(event.value)}
+            {...rest}
+        />
+        {meta.error && meta.touched && <span className="k-required">{meta.error}</span>}
+    </label>;
+};
+
+const KendoInput = (options) => (kendoHOC(Input, options))
+const KendoNumericTextBox = (options) => (kendoHOC(NumericTextBox, options))
+const KendoDatePicker = (options) => (kendoHOC(DatePicker, options))
+const KendoDropDown = (options) => (kendoHOC(DropDownList, options))
+const KendoSwitch = (options) => (kendoHOC(Switch, options))
 
 let ReduxProductsForm = props => {
   const { handleSubmit, isNew } = props
@@ -25,22 +42,47 @@ let ReduxProductsForm = props => {
       <div className="header">
         <h5>Product</h5>
       </div>
-      <form onSubmit={handleSubmit} className="k-form">
+      <form className="k-form" >
         <fieldset>
-          <Field name="ProductID" component={renderField} type="number" label={"ProductID"} />
-          <Field name="ProductName" component={renderField} type="text" label={"Product Name"} validate={[required]} />
-          <Field name="UnitPrice" component={renderField} type="number" label={"Unit Price"} validate={[required, minValue1]} />
-          <Field name="UnitsInStock" component={renderField} type="number" label={"Units In Stock"} validate={[required, minValue0]} />
+          <Field
+            name="ProductID"
+            component={KendoInput}
+            type="number"
+            label={"ProductID"}
+            disabled={true}
+          />
+          <Field
+            name="ProductName"
+            component={KendoInput}
+            type="text"
+            label={"Product Name"}
+            validate={[required]} />
+          <Field
+            name="UnitPrice"
+            component={KendoNumericTextBox}
+            type="number"
+            label={"Unit Price"}
+            validate={[required, minValue1]}
+          />
+          <Field
+            name="UnitsInStock"
+            component={KendoNumericTextBox}
+            type="number"
+            label={"Units In Stock"}
+            validate={[required, minValue0]}
+          />
           <div className="text-right">
-            <button
+            <Button
               type="submit"
-              className={"k-button k-primary" + (isNew ? " k-outline" : "")}
+              onClick={handleSubmit}
+              primary={true}
+              look={isNew ? 'outline'  : null}
             >
               {isNew
                 ? "Add new product"
                 : "Update"
               }
-            </button>
+            </Button>
           </div>
         </fieldset>
       </form>
@@ -54,7 +96,7 @@ ReduxProductsForm = reduxForm({
 
 ReduxProductsForm = connect(
   state => ({
-    initialValues: {},
+    initialValues: {UnitPrice: 0, UnitsInStock: 0},
     isNew: state.selectionReducer.isNew
   })
 
